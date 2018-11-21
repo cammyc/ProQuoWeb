6a67f796e893c4dfce33128d83a4a47d6d47a953
cammyc <camconnor0903@gmail.com>
2018-11-21 14:55:18 -0700

    Stripe updates

--- scalpr_ws/get_user_details_stripe.php
+++ scalpr_ws/get_user_details_stripe.php
@@ -16,23 +16,31 @@
 		$stripeDetails = getStripeAccount($mysqli, $userID); //get stripe specific details
 
 		if(!empty($stripeDetails->connectID)){ //check if user has a registered fund receival method
-			$array = \Stripe\Account::retrieve($stripeDetails->connectID)->external_accounts->all(array(
-	  		'limit'=>1, 'object' => $stripeDetails->paymentType));
+			try {
+				$array = \Stripe\Account::retrieve($stripeDetails->connectID)->external_accounts->all(array(
+		  		'limit'=>1, 'object' => $stripeDetails->paymentType));
 
-		  	if (strcasecmp($stripeDetails->receivalType, "card") == 0){
-		  		$stripeDetails->receivalPreview = $array->data[0]->last4; //if card preview is last 4 numbers
-		  	}else{
-		  		
-		  	}
+			  	if (strcasecmp($stripeDetails->receivalType, "card") == 0){
+			  		$stripeDetails->receivalPreview = $array->data[0]->last4; //if card preview is last 4 numbers
+			  	}else{
+			  		
+			  	}
+			} catch(\Stripe\Error $e) {
+				//dont show preview and allow user to add new payment method
+			}
 		}
 
 		if(!empty($stripeDetails->customerID)){ //payment
-			$cus = \Stripe\Customer::retrieve($stripeDetails->customerID);// check if user has a registered payment method
+			try{
+				$cus = \Stripe\Customer::retrieve($stripeDetails->customerID);// check if user has a registered payment method
 
-			if (strcasecmp($stripeDetails->paymentType, "card") == 0){
-				$stripeDetails->paymentPreview = $array->data[0]->last4; //if card preview is last 4 numbers
-			}else{
+				if (strcasecmp($stripeDetails->paymentType, "card") == 0){
+					$stripeDetails->paymentPreview = $array->data[0]->last4; //if card preview is last 4 numbers
+				}else{
 
+				}
+			} catch(\Stripe\Error $e) {
+				//dont show preview and allow user to add new payment method
 			}
 		}
 
